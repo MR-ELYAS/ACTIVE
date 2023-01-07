38a23cc195e933e78b2fc8a012af4412441b72d6
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-01-07 14:52:32 +0430

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 import os
 import sys
+import time
+import datetime
 
-print("RANING..")
-os.system("https://t.me/KingMazar")
+print("\033[1;32mRANING..")
+time.sleep(2)
+os.system("xdg-open https://t.me/KingMazar")
+
+print("\033[1;32mYOU CAN USE /ACTIVE")
 
 import ACTIVE
